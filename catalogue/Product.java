c92de4fb8c375068d8b7de5d96803b33bd9f62c9
BigNeeko <127303414+BigNeeko@users.noreply.github.com>
2024-01-11 10:36:17 +0000

    Update Product.java

--- catalogue/Product.java
+++ catalogue/Product.java
@@ -2,13 +2,7 @@ package catalogue;
 
 import java.io.Serializable;
 
-/**
- * Used to hold the following information about
- * a product: Product number, Description, Price and
- * Stock level.
- * @author  Mike Smith University of Brighton
- * @version 2.0
- */
+
 
 public class Product implements Serializable
 {
@@ -18,13 +12,7 @@ public class Product implements Serializable
   private double thePrice;            // Price of product
   private int    theQuantity;         // Quantity involved
 
-  /**
-   * Construct a product details
-   * @param aProductNum Product number
-   * @param aDescription Description of product
-   * @param aPrice The price of the product
-   * @param aQuantity The Quantity of the product involved
-   */
+
   public Product( String aProductNum, String aDescription,
                   double aPrice, int aQuantity )
   {
@@ -53,9 +41,9 @@ public class Product implements Serializable
   { 
     thePrice = aPrice;
   }
-  
+
   public void setQuantity( int aQuantity )
-  { 
+  {
     theQuantity = aQuantity;
   }
 
