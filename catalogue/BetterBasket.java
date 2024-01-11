2e8d088bc2848e1505023cde74c5f1124a5c2e01
BigNeeko <127303414+BigNeeko@users.noreply.github.com>
2024-01-11 10:36:00 +0000

    Update BetterBasket.java

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -2,17 +2,42 @@ package catalogue;
 
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Write a description of class BetterBasket here.
- * 
- * @author  Your Name 
+ *
+ * @author  Your Name
  * @version 1.0
  */
-public class BetterBasket extends Basket implements Serializable
-{
+public class BetterBasket extends Basket implements Serializable {
   private static final long serialVersionUID = 1L;
-  
 
-  // You need to add code here
+  @Override
+  public boolean add(Product p1) {
+
+    for (Product p2 : this) {
+      if (p1.getProductNum().equals(p2.getProductNum())) {
+        p2.setQuantity(p2.getQuantity() + p1.getQuantity());
+        return (true);
+      }
+    }
+
+    super.add(p1);
+    return (true);
+  }
+
+  public boolean removeProduct(String productNum) {
+    for (Product pr : this) {
+      if (pr.getProductNum().equals(productNum)) {
+        return super.remove(pr);
+      }
+    }
+    return false;  // Product not found in the basket
+  }
+
+
+
+
 }
